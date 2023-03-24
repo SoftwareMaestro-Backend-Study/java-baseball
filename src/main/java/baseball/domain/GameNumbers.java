b0b21a581c19bf0aa97f82e5add7526f9d312f14
seong-wooo <seongwoo0513@uos.ac.kr>
2023-03-24 16:55:21 +0900

    test: GameNumbersTest 테스트 케이스 추가

--- src/main/java/baseball/domain/GameNumbers.java
+++ src/main/java/baseball/domain/GameNumbers.java
@@ -2,6 +2,8 @@ package baseball.domain;
 
 import baseball.domain.gamenumbercreator.GameNumberCreator;
 import java.util.List;
+import java.util.Objects;
+import java.util.stream.IntStream;
 
 public class GameNumbers {
 
@@ -18,7 +20,7 @@ public class GameNumbers {
     }
 
     private void validateLength(List<GameNumber> numbers) {
-        if (numbers.size() > MAX_LENGTH) {
+        if (numbers.size() != MAX_LENGTH) {
             throw new IllegalArgumentException(String.format("[ERROR] 게임 숫자는 %d 자리를 초과할 수 없습니다.", MAX_LENGTH));
         }
     }
--- src/test/java/baseball/domain/GameNumbersTest.java
+++ src/test/java/baseball/domain/GameNumbersTest.java
@@ -4,18 +4,27 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import baseball.domain.gamenumbercreator.GameNumberCreator;
 import java.util.List;
-import org.junit.jupiter.api.Test;
+import java.util.stream.Stream;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 class GameNumbersTest {
 
-    @Test
-    void 게임_넘버가_3자리를_초과하면_예외를_던진다() {
+    public static Stream<Arguments> invalidGameNumbers() {
+        return Stream.of(
+                Arguments.of(List.of(new GameNumber(1), new GameNumber(2), new GameNumber(3), new GameNumber(4))),
+                Arguments.of(List.of(new GameNumber(1), new GameNumber(2)))
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("invalidGameNumbers")
+    void 게임_넘버가_3자리가_아니면_예외를_던진다(List<GameNumber> gameNumbers) {
         // given
-        GameNumberCreator creator = () ->
-                List.of(new GameNumber(1), new GameNumber(2), new GameNumber(3), new GameNumber(4));
+        GameNumberCreator creator = () -> gameNumbers;
 
         assertThatThrownBy(() -> GameNumbers.from(creator))
                 .isInstanceOf(IllegalArgumentException.class);
     }
-
 }
