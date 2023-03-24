3d209adc8477d149cb18035ca7d72b54a34b207a
seong-wooo <seongwoo0513@uos.ac.kr>
2023-03-24 15:56:39 +0900

    feat: Integer값을 게임 수로 변경하는 기능 구현 및 테스트

---
+++ src/main/java/baseball/domain/IntegerToGameNumberCreator.java
@@ -0,0 +1,19 @@
+package baseball.domain;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class IntegerToGameNumberCreator implements GameNumberCreator {
+    private final List<Integer> values;
+
+    public IntegerToGameNumberCreator(List<Integer> values) {
+        this.values = values;
+    }
+
+    @Override
+    public List<GameNumber> create() {
+        return values.stream()
+                .map(GameNumber::new)
+                .collect(Collectors.toList());
+    }
+}
---
+++ src/test/java/baseball/domain/IntegerToGameNumberCreatorTest.java
@@ -0,0 +1,19 @@
+package baseball.domain;
+
+import java.util.List;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class IntegerToGameNumberCreatorTest {
+
+    @Test
+    void 숫자들을_통해_게임_숫자를_만들_수_있다() {
+        // given
+        final List<Integer> values = List.of(1, 2, 3, 4, 5, 6, 7, 8, 9);
+        final IntegerToGameNumberCreator creator = new IntegerToGameNumberCreator(values);
+
+        // when & then
+        Assertions.assertThatCode(creator::create)
+                .doesNotThrowAnyException();
+    }
+}
