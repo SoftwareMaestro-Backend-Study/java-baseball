ca9e7afff2802ba29b08eb5c417cfd59077ac91f
seong-wooo <seongwoo0513@uos.ac.kr>
2023-03-24 14:21:29 +0900

    feat: 게임 숫자 생성 및 테스트

---
+++ src/main/java/baseball/domain/GameNumber.java
@@ -0,0 +1,39 @@
+package baseball.domain;
+
+import java.util.Objects;
+
+public class GameNumber {
+
+    private static final int MIN_VALUE = 1;
+    private static final int MAX_VALUE = 9;
+
+    private final int value;
+
+    public GameNumber(int value) {
+        validateRange(value);
+        this.value = value;
+    }
+
+    private void validateRange(int value) {
+        if (value < MIN_VALUE || value > MAX_VALUE) {
+            throw new IllegalArgumentException(String.format("[ERROR] 야구게임 숫자는 %d일 수 없습니다", value));
+        }
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        GameNumber that = (GameNumber) o;
+        return value == that.value;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(value);
+    }
+}
---
+++ src/test/java/baseball/domain/GameNumberTest.java
@@ -0,0 +1,22 @@
+package baseball.domain;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+class GameNumberTest {
+
+    @ParameterizedTest
+    @ValueSource(ints = {0, 10})
+    void 게임_숫자가_1이상_9이하의_자연수가_아니면_예외를_던진다(int number) {
+        Assertions.assertThatThrownBy(() -> new GameNumber(number))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {1, 2, 3, 4, 5, 6, 7, 8, 9})
+    void 게임_숫자는_1이상_9이하의_자연수여야한다(int number) {
+        Assertions.assertThatCode(() -> new GameNumber(number))
+                .doesNotThrowAnyException();
+    }
+}
