dcc759fcec847e6ec184f3c7d44e0d2888f8a3cd
raycho <raycho521@yonsei.ac.kr>
2023-03-25 19:56:54 +0900

    feat : implement UserInput

--- src/main/java/baseball/inout/UserInput.java
+++ src/main/java/baseball/inout/UserInput.java
@@ -1,6 +1,7 @@
 package baseball.inout;
 
 
+import java.util.ArrayList;
 import java.util.List;
 
 import static camp.nextstep.edu.missionutils.Console.readLine;
@@ -12,18 +13,26 @@ public class UserInput {
      */
     public List<Integer> getNum() {
         System.out.print("숫자를 입력해주세요 : ");
-        String input = readLine();
+        String input = readLine().trim();
 
 //        CheckRegex.isPlayball
-//        Integer.parseInt(input);
-        return List.of(0,0,0);
+
+        List<Integer> numbers = new ArrayList<>();
+        for (int i = 0; i < 3; i++)
+            numbers.add(input.charAt(i) - '0');
+
+        return numbers;
     }
 
     /**
      * 게임 진행여부 입력
      */
     public boolean isContinue() {
-        return false;
+        System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+        String input = readLine().trim();
+        // regex
+
+        return input.equals("1");
     }
 
 }
