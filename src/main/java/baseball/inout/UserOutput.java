483d07344af55b0eda2cd81d9f7a8fefd4fc8efc
raycho <raycho521@yonsei.ac.kr>
2023-03-25 19:57:02 +0900

    feat : implement UserOutput

--- src/main/java/baseball/inout/UserOutput.java
+++ src/main/java/baseball/inout/UserOutput.java
@@ -4,16 +4,24 @@ import baseball.game.GameStatus;
 
 public class UserOutput {
 
-    public void initMessage(){
+    public void initMessage() {
+        System.out.println("숫자 야구 게임을 시작합니다.");
+    }
+
+    public void statusMessage(GameStatus status) {
+        if (status.ball == 0 && status.strike == 0)
+            System.out.println("낫싱");
+        else if (status.ball == 0)
+            System.out.println(status.strike + "스트라이크");
+        else if (status.strike == 0)
+            System.out.println(status.ball + "볼");
+        else
+            System.out.println(status.ball + "볼 " + status.strike + "스트라이크");
 
     }
 
-    public void statusMessage(GameStatus status){
-
-    }
-
-    public void endMessage(){
-
+    public void endMessage() {
+        System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
     }
 
 }
