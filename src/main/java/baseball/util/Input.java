5b5039689c711553c0f36ff3a604a2565c16f196
jeonyuneo <piq2255@gmail.com>
2023-03-25 21:44:52 +0900

    refactor: Input - 공백 및 대체문자 상수화

--- src/main/java/baseball/util/Input.java
+++ src/main/java/baseball/util/Input.java
@@ -4,11 +4,18 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class Input {
 
+    private static final String BLANK = " ";
+    private static final String DELETE = "";
+
     private Input() {
     }
 
     public static String readGameNumber() {
         System.out.print("숫자를 입력해주세요 : ");
-        return Console.readLine();
+        return removeBlank(Console.readLine());
+    }
+
+    private static String removeBlank(String input) {
+        return input.replaceAll(BLANK, DELETE);
     }
 }
