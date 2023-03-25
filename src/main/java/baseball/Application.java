25852e93e810d89ea2d394bf13229af1a24274d6
jeonyuneo <piq2255@gmail.com>
2023-03-25 17:57:24 +0900

    feat: 게임 이어하기 및 종료 기능 추가

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -17,45 +17,50 @@ public class Application {
                 computer.add(randomNumber);
             }
         }
-        String input = Console.readLine();
-        String inputWithoutBlank = input.replaceAll(" ", "");
-        if (inputWithoutBlank.length() != 3) {
-            throw new IllegalArgumentException("3자리 값이 아닙니다.");
-        }
-        for (int i = 0; i < 3; i++) {
-            int each = inputWithoutBlank.charAt(i) - '0';
-            if (each < 0 || each > 9) {
-                throw new IllegalArgumentException("정수가 아닌 값이 포함되어 있습니다.");
+        while (true) {
+            String input = Console.readLine();
+            String inputWithoutBlank = input.replaceAll(" ", "");
+            if (inputWithoutBlank.length() != 3) {
+                throw new IllegalArgumentException("3자리 값이 아닙니다.");
             }
-            if (each == 0) {
-                throw new IllegalArgumentException("1~9 범위를 벗어나는 숫자가 포함되어 있습니다.");
+            for (int i = 0; i < 3; i++) {
+                int each = inputWithoutBlank.charAt(i) - '0';
+                if (each < 0 || each > 9) {
+                    throw new IllegalArgumentException("정수가 아닌 값이 포함되어 있습니다.");
+                }
+                if (each == 0) {
+                    throw new IllegalArgumentException("1~9 범위를 벗어나는 숫자가 포함되어 있습니다.");
+                }
             }
-        }
-        int[] comparingResult = new int[3];
-        for (int i = 0; i < 3; i++) {
-            int number = inputWithoutBlank.charAt(i) - '0';
-            if (computer.contains(number) && computer.indexOf(number) != i) {
-                comparingResult[0]++;
-            } else if (computer.contains(number) && computer.indexOf(number) == i) {
-                comparingResult[1]++;
+            int[] comparingResult = new int[3];
+            for (int i = 0; i < 3; i++) {
+                int number = inputWithoutBlank.charAt(i) - '0';
+                if (computer.contains(number) && computer.indexOf(number) != i) {
+                    comparingResult[0]++;
+                } else if (computer.contains(number) && computer.indexOf(number) == i) {
+                    comparingResult[1]++;
+                } else {
+                    comparingResult[2]++;
+                }
+            }
+            StringBuilder result = new StringBuilder();
+            if (comparingResult[2] == 3) {
+                result.append("낫싱");
             } else {
-                comparingResult[2]++;
+                if (comparingResult[0] != 0) {
+                    result.append(comparingResult[0]).append("볼 ");
+                }
+                if (comparingResult[1] != 0) {
+                    result.append(comparingResult[1]).append("스트라이크");
+                }
+            }
+            if (comparingResult[1] == 3) {
+                result.append("\n3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+            }
+            System.out.println(result);
+            if (comparingResult[1] == 3) {
+                break;
             }
         }
-        StringBuilder result = new StringBuilder();
-        if (comparingResult[2] == 3) {
-            result.append("낫싱");
-        } else {
-            if (comparingResult[0] != 0) {
-                result.append(comparingResult[0]).append("볼 ");
-            }
-            if (comparingResult[1] != 0) {
-                result.append(comparingResult[1]).append("스트라이크");
-            }
-        }
-        if (comparingResult[1] == 3) {
-            result.append("\n3개의 숫자를 모두 맞히셨습니다! 게임 종료");
-        }
-        System.out.println(result);
     }
 }
