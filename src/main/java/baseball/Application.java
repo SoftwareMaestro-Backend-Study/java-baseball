6a580a17a38758fe881b13aa332c4ef4416982a6
raycho <raycho521@yonsei.ac.kr>
2023-03-25 19:57:13 +0900

    feat : implement Application

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,44 @@
 package baseball;
 
+import baseball.game.GameStatus;
+import baseball.game.SingleGame;
+import baseball.inout.UserInput;
+import baseball.inout.UserOutput;
+
+import java.util.List;
+
 public class Application {
+    private static UserInput userInput;
+    private static UserOutput userOutput;
+
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        boolean isContinue = true;
+
+        startGame();
+        while (isContinue)
+            isContinue = playSingleGame();
+    }
+
+    private static void startGame() {
+        userInput = new UserInput();
+        userOutput = new UserOutput();
+
+        userOutput.initMessage();
+    }
+
+    private static boolean playSingleGame() {
+        SingleGame singleGame = new SingleGame();
+
+        boolean isCorrect = false;
+
+        while (!isCorrect) {
+            List<Integer> playNum = userInput.getNum();
+            GameStatus gameStatus = singleGame.singleTurn(playNum);
+            userOutput.statusMessage(gameStatus);
+            isCorrect = gameStatus.isCorrect();
+        }
+
+        userOutput.endMessage();
+        return userInput.isContinue();
     }
 }
