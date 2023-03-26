d25a2d5d48ef5e8fdee38b0a9a0fce5f342c4c37
jeonyuneo <piq2255@gmail.com>
2023-03-26 19:07:52 +0900

    refactor: GameNumber - 검증 로직 추가

--- src/main/java/baseball/domain/GameNumber.java
+++ src/main/java/baseball/domain/GameNumber.java
@@ -2,13 +2,23 @@ package baseball.domain;
 
 public class GameNumber {
 
-    private int value;
+    private static final int MIN_VALUE = 1;
+    private static final int MAX_VALUE = 9;
+
+    private final int value;
 
     public GameNumber(int value) {
+        validateRange(value);
         this.value = value;
     }
 
     public int getValue() {
         return value;
+    }
+
+    private static void validateRange(int value) {
+        if (value < MIN_VALUE || value > MAX_VALUE) {
+            throw new IllegalArgumentException("1~9 범위를 벗어나는 숫자가 포함되어 있습니다.");
+        }
     }
 }
--- src/main/java/baseball/util/GameNumberGenerator.java
+++ src/main/java/baseball/util/GameNumberGenerator.java
@@ -3,37 +3,21 @@ package baseball.util;
 import baseball.domain.GameNumber;
 import baseball.domain.GameNumbers;
 
-import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class GameNumberGenerator {
 
-    private static final int THE_NUMBER_OF_GAME_NUMBER = 3;
     private static final String DELIMITER = "";
 
     private GameNumberGenerator() {
     }
 
     public static GameNumbers generate(String input) {
-        List<GameNumber> gameNumbers = new ArrayList<>();
-        validateTheNumberOf(input);
-        for (String each : input.split(DELIMITER)) {
-            int gameNumber = Convertor.toInteger(each);
-            validateRange(gameNumber);
-            gameNumbers.add(new GameNumber(gameNumber));
-        }
+        List<GameNumber> gameNumbers = Arrays.stream(input.split(DELIMITER))
+                .map(each -> new GameNumber(Convertor.toInteger(each)))
+                .collect(Collectors.toList());
         return new GameNumbers(gameNumbers);
-    }
-
-    private static void validateTheNumberOf(String input) {
-        if (input.length() != THE_NUMBER_OF_GAME_NUMBER) {
-            throw new IllegalArgumentException("3자리 값이 아닙니다.");
-        }
-    }
-
-    private static void validateRange(int each) {
-        if (each == 0) {
-            throw new IllegalArgumentException("1~9 범위를 벗어나는 숫자가 포함되어 있습니다.");
-        }
     }
 }
