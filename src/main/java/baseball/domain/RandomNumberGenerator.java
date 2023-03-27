885f15664ab726694c84332deb6a259516268876
jeonyuneo <piq2255@gmail.com>
2023-03-27 22:30:30 +0900

    style: RandomNumberGenerator - 오타 삭제

--- src/main/java/baseball/domain/RandomNumberGenerator.java
+++ src/main/java/baseball/domain/RandomNumberGenerator.java
@@ -4,7 +4,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
-public class RandomNumberGenerator및 implements GameNumberGenerator {
+public class RandomNumberGenerator implements GameNumberGenerator {
 
     public List<GameNumber> generate(int limit) {
         return Stream.generate(GameNumber::create)
