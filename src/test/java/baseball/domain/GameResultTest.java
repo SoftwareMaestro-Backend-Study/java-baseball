88990f2b15e849d19e914406735efc20297dc360
jeonyuneo <piq2255@gmail.com>
2023-03-27 22:55:23 +0900

    test: GameResultTest - 테스트 코드 수정

--- src/test/java/baseball/domain/GameResultTest.java
+++ src/test/java/baseball/domain/GameResultTest.java
@@ -7,39 +7,26 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class GameResultTest {
 
+    private static final int LIMIT = 3;
+
     @DisplayName("같은 수가 없으면 낫싱 출력")
     @Test
     void 같은_수_없으면_낫싱() {
-        // given
-        int[] comparingResult = {0, 0, 3};
-        // when
-        GameResult gameResult = GameResult.from(comparingResult);
-        // then
-        assertThat(gameResult.getValue())
+        assertThat(GameResult.from(0, 0, LIMIT).getValue())
                 .isEqualTo("낫싱");
     }
 
     @DisplayName("같은 수가 같은 자리에 있으면 스트라이크")
     @Test
     void 같은_수_같은_자리면_스트라이크() {
-        // given
-        int[] comparingResult = {0, 2, 0};
-        // when
-        GameResult gameResult = GameResult.from(comparingResult);
-        // then
-        assertThat(gameResult.getValue())
+        assertThat(GameResult.from(2, 0, LIMIT).getValue())
                 .isEqualTo("2스트라이크");
     }
 
     @DisplayName("같은 수가 다른 자리에 있으면 볼")
     @Test
     void 같은_수_다른_자리면_볼() {
-        // given
-        int[] comparingResult = {1, 0, 0};
-        // when
-        GameResult gameResult = GameResult.from(comparingResult);
-        // then
-        assertThat(gameResult.getValue())
+        assertThat(GameResult.from(0, 1, LIMIT).getValue())
                 .isEqualTo("1볼 ");
     }
 }
