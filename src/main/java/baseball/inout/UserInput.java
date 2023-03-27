fec88027f035fe24396cd74244112fde8450d086
raycho <raycho521@yonsei.ac.kr>
2023-03-27 20:44:36 +0900

    refactor : edit UserInput return type

--- src/main/java/baseball/inout/UserInput.java
+++ src/main/java/baseball/inout/UserInput.java
@@ -1,11 +1,6 @@
 package baseball.inout;
 
 
-import baseball.util.CheckRegex;
-
-import java.util.ArrayList;
-import java.util.List;
-
 import static camp.nextstep.edu.missionutils.Console.readLine;
 
 public class UserInput {
@@ -17,17 +12,6 @@ public class UserInput {
         System.out.print("숫자를 입력해주세요 : ");
         String input = readLine().trim();
         return input;
-
-//        if (!CheckRegex.isThreeDigit(input))
-//            throw new IllegalArgumentException("3자리의 숫자를 입력해야 합니다.");
-//        if (!CheckRegex.isBaseball(input))
-//            throw new IllegalArgumentException("중복이 없는 3자리의 숫자를 입력해야 합니다.");
-//
-//        List<Integer> numbers = new ArrayList<>();
-//        for (int i = 0; i < 3; i++)
-//            numbers.add(input.charAt(i) - '0');
-//
-//        return numbers;
     }
 
     /**
@@ -37,12 +21,6 @@ public class UserInput {
         System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
         String input = readLine().trim();
         return input;
-
-//        // regex
-//        if (!CheckRegex.isCommand(input))
-//            throw new IllegalArgumentException("1 또는 2를 입력해야 합니다.");
-//
-//        return input.equals("1");
     }
 
 }
