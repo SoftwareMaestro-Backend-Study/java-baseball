3198cf6960cc424dbfc94ff225928b097420c2a6
raycho <raycho521@yonsei.ac.kr>
2023-03-27 20:16:07 +0900

    refactor : edit output type

--- src/main/java/baseball/inout/UserInput.java
+++ src/main/java/baseball/inout/UserInput.java
@@ -1,6 +1,8 @@
 package baseball.inout;
 
 
+import baseball.util.CheckRegex;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -11,33 +13,36 @@ public class UserInput {
     /**
      * 숫자 입력
      */
-    public List<Integer> getNum() {
+    public String getNum() {
         System.out.print("숫자를 입력해주세요 : ");
         String input = readLine().trim();
+        return input;
 
-        if (!CheckRegex.isThreeDigit(input))
-            throw new IllegalArgumentException("3자리의 숫자를 입력해야 합니다.");
-        if (!CheckRegex.isBaseball(input))
-            throw new IllegalArgumentException("중복이 없는 3자리의 숫자를 입력해야 합니다.");
-
-        List<Integer> numbers = new ArrayList<>();
-        for (int i = 0; i < 3; i++)
-            numbers.add(input.charAt(i) - '0');
-
-        return numbers;
+//        if (!CheckRegex.isThreeDigit(input))
+//            throw new IllegalArgumentException("3자리의 숫자를 입력해야 합니다.");
+//        if (!CheckRegex.isBaseball(input))
+//            throw new IllegalArgumentException("중복이 없는 3자리의 숫자를 입력해야 합니다.");
+//
+//        List<Integer> numbers = new ArrayList<>();
+//        for (int i = 0; i < 3; i++)
+//            numbers.add(input.charAt(i) - '0');
+//
+//        return numbers;
     }
 
     /**
      * 게임 진행여부 입력
      */
-    public boolean isContinue() {
+    public String isContinue() {
         System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
         String input = readLine().trim();
-        // regex
-        if (!CheckRegex.isCommand(input))
-            throw new IllegalArgumentException("1 또는 2를 입력해야 합니다.");
+        return input;
 
-        return input.equals("1");
+//        // regex
+//        if (!CheckRegex.isCommand(input))
+//            throw new IllegalArgumentException("1 또는 2를 입력해야 합니다.");
+//
+//        return input.equals("1");
     }
 
 }
