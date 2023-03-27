aaa90c8de9e13c292d23d90d630b556d5efac4b3
jeonyuneo <piq2255@gmail.com>
2023-03-27 22:52:06 +0900

    test: UserNumberGeneratorTest - 패키지 이동 및 테스트 코드 수정

--- src/test/java/baseball/domain/UserNumberGeneratorTest.java
+++ src/test/java/baseball/domain/UserNumberGeneratorTest.java
@@ -1,5 +1,6 @@
-package baseball.util;
+package baseball.domain;
 
+import baseball.domain.GameNumberGenerator;
 import baseball.domain.UserNumberGenerator;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
@@ -10,11 +11,16 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class UserNumberGeneratorTest {
 
+    private static final int LIMIT = 3;
+
     @DisplayName("형식 외 입력 시 예외 발생")
     @ParameterizedTest
-    @ValueSource(strings = {"1234", "12-", "120", "122"})
+    @ValueSource(strings = {"1234", "12-", "120"})
     void 잘못된_입력(String input) {
-        assertThatThrownBy(() -> UserNumberGenerator.generate(input))
+        // given
+        GameNumberGenerator userNumberGenerator = new UserNumberGenerator(input);
+        // when & then
+        assertThatThrownBy(() -> userNumberGenerator.generate(LIMIT))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
@@ -22,7 +28,10 @@ public class UserNumberGeneratorTest {
     @ParameterizedTest
     @ValueSource(strings = {"123", "483", "679"})
     void 올바른_입력(String input) {
-        assertThatCode(() -> UserNumberGenerator.generate(3))
+        // given
+        GameNumberGenerator userNumberGenerator = new UserNumberGenerator(input);
+        // when & then
+        assertThatCode(() -> userNumberGenerator.generate(LIMIT))
                 .doesNotThrowAnyException();
     }
 }
